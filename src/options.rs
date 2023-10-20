76ed90899d2ae40b68f0ce2bf10fa95d3e0a9678
Jeroen Vannevel <jer_vannevel@outlook.com>
2023-10-20 20:14:10 +0100

    fix documentation

--- src/options.rs
+++ src/options.rs
@@ -6,7 +6,7 @@ pub struct Options {
     #[structopt(parse(from_os_str))]
     pub path: std::path::PathBuf,
 
-    /// AWS region (backup)
+    /// Log level
     #[structopt(default_value = "info", short, long)]
     pub log_level: String,
 }
